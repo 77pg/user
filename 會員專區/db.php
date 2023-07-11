d0453bd1f89b559556d2c27ac8db75747bbedd8c
77 <qiqi77pg@gmail.com>
2023-07-12 00:57:20 +0800

    成功加入到資料庫(token、登入登出)

--- 會員專區/db.php
+++ 會員專區/db.php
@@ -3,9 +3,11 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 // 連結資料庫跟sql.php
 $host = "localhost";
+// 不建議用root，有資安危險，可以用abuser
 $user = "root";
+// 密碼1234
 $pwd = "";
-$db = "addressbook";
+$db = "charites";
 
 $mysqli = new mysqli($host, $user, $pwd, $db);
 ?>

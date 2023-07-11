f4b6537bbfcc27bf556f9bd88e85fa37c9b0783a
77 <qiqi77pg@gmail.com>
2023-07-11 16:27:47 +0800

    SQL

---
+++ 會員專區/charites.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 2023 年 07 月 11 日 03:39
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `charites`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `activity`
+--
+
+CREATE TABLE `activity` (
+  `aid` varchar(20) NOT NULL,
+  `pid` varchar(20) NOT NULL,
+  `aname` varchar(50) NOT NULL,
+  `discount` int(11) NOT NULL,
+  `a_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` varchar(20) NOT NULL,
+  `uid` varchar(50) NOT NULL,
+  `pid` varchar(20) NOT NULL,
+  `pname` varchar(20) NOT NULL,
+  `count` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `gender`
+--
+
+CREATE TABLE `gender` (
+  `gid` int(11) NOT NULL,
+  `gender` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `gender`
+--
+
+INSERT INTO `gender` (`gid`, `gender`) VALUES
+(2, 'female'),
+(1, 'male');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` varchar(20) NOT NULL,
+  `cart_id` varchar(20) NOT NULL,
+  `o_count` int(11) NOT NULL,
+  `o_total` int(11) NOT NULL,
+  `o_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `product`
+--
+
+CREATE TABLE `product` (
+  `pid` varchar(20) NOT NULL,
+  `pimage` mediumblob DEFAULT NULL,
+  `pname` varchar(20) NOT NULL,
+  `species_id` int(11) NOT NULL,
+  `pcontent` varchar(50) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `species`
+--
+
+CREATE TABLE `species` (
+  `species_id` int(11) NOT NULL,
+  `species` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `user`
+--
+
+CREATE TABLE `user` (
+  `uid` varchar(50) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `cname` varchar(50) NOT NULL,
+  `pwd` varchar(50) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `user`
+--
+
+INSERT INTO `user` (`uid`, `gender`, `cname`, `pwd`, `email`, `phone`, `address`) VALUES
+('A01', 'male', '王大明', '1234', 'daming01@example.com', 1111, '台北市中山路一段1號');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- 資料表索引 `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`);
+
+--
+-- 資料表索引 `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`gid`),
+  ADD KEY `gender` (`gender`);
+
+--
+-- 資料表索引 `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- 資料表索引 `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- 資料表索引 `species`
+--
+ALTER TABLE `species`
+  ADD PRIMARY KEY (`species_id`);
+
+--
+-- 資料表索引 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`),
+  ADD KEY `gender` (`gender`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `gender`
+--
+ALTER TABLE `gender`
+  MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `species`
+--
+ALTER TABLE `species`
+  MODIFY `species_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`gender`) REFERENCES `gender` (`gender`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
